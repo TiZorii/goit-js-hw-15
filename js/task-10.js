489707f3a4360cbd7d98f8a78d9bea9007071d61
Tetiana Zorii <tanyusha.zoriy@gmail.com>
2023-10-20 19:50:06 -0400

    changes in task 10

--- js/task-10.js
+++ js/task-10.js
@@ -9,14 +9,24 @@ const createButton = document.querySelector('button[data-create]');
 const destroyButton = document.querySelector('button[data-destroy]');
 const boxesDiv = document.querySelector('#boxes');
 
-createBtn.addEventListener('click', handleCreateBoxes);
-destroyBtn.addEventListener('click', handleDestroyBoxes);
+createButton.addEventListener('click', handleCreateBoxes);
+destroyButton.addEventListener('click', handleDestroyBoxes);
 
 function handleCreateBoxes() {
   const amount = input.value;
+  createBoxes(amount);
+}
+function handleDestroyBoxes() {
   destroyBoxes();
+}
 
-  const elements = [];
+function destroyBoxes() {
+  boxesDiv.innerHTML = '';
+}
+
+
+function createBoxes(amount) {
+const elements = [];
   let size = 30;
 
   for (let i = 0; i < amount; i += 1) {
@@ -27,14 +37,6 @@ function handleCreateBoxes() {
     elements.push(box);
     size += 10;
   }
-  boxesContainer.append(...elements);
+  boxesDiv.append(...elements);
 }
-function handleDestroyBoxes() {
-  destroyBoxes();
-}
-
-function destroyBoxes() {
-  boxesContainer.innerHTML = '';
-}
-
 
